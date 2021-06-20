84dabbc21af148b25eb23164c157eb592a0ab315
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-06-20 08:04:48 +0700

    Add files via upload

---
+++ plugins/donasi.js
@@ -0,0 +1,12 @@
+let handler = async m => m.reply(`
+╭─「 Donasi  」
+│ • Gopay : [6281215199447]
+│ • Indosat Ooredoo [6285642337380]
+│ • Telkomsel [081215199447]
+╰────
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
