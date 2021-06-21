28697f8e9bfe60621b0db51bf4bb2692da591faf
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-06-21 08:52:42 +0700

    Update ssweb.js

--- plugins/ssweb.js
+++ plugins/ssweb.js
@@ -3,8 +3,8 @@ let handler = async (m, { conn, command, args }) => {
   let full = /f$/i.test(command)
   if (!args[0]) return conn.reply(m.chat, 'Tidak ada url', m)
   let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
-  let ss =  await require('../lib/ssweb')(url, full)
-  conn.sendMessage(m.chat, ss, 'imageMessage', { quoted: m })
+  let ss =  `https://api-caliph.herokuapp.com/api/ssweb?url=${url}&full=${full}&quality=1000&delay=1000`
+  conn.sendMessage(m.chat, await fetch(ss).then(v => v.buffer()), 'imageMessage', { quoted: m })
 }
 handler.help = ['ss', 'ssf'].map(v => v + ' <url>')
 handler.tags = ['internet']
